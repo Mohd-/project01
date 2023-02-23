1fa255a9a330cbb8ca773929e808e996b8744d84
Mohd- <shinuobu@gmail.com>
2023-02-23 19:58:01 +0300

    modified:   mysite/tickets/models.py

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone_no = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+class Client(User):
+    tier = 'inactive'
+    def __str__(self):
+        return self.name
