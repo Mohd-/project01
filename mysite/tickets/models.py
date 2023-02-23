0b5206d22436a52c9e467816d7e94d9c06c85f15
Mohd- <shinuobu@gmail.com>
2023-02-23 20:14:05 +0300

    Added Client, Customer, Reservation models

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -6,8 +6,22 @@ class User(models.Model):
     phone_no = models.CharField(max_length=200)
     def __str__(self):
         return self.name
+
+
     
 class Client(User):
     tier = 'inactive'
+    def create_reservation(title, date_time, capacity):
+        return Reservation(title, date_time, capacity)
     def __str__(self):
         return self.name
+
+class Customer(User):
+    def __str__(self):
+        return self.name
+    
+class Reservation(models.Model):
+    title = models.CharField(max_length=450)
+    date_time = models.DateTimeField('date and time of the event')
+    capacity = models.IntegerField(default=0)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
