54ce21559481d8ec74e378a127252d95ad312ac8
Mohd- <shinuobu@gmail.com>
2023-02-23 20:22:57 +0300

    modified:   mysite/tickets/models.py

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -7,16 +7,6 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
-
-    
-class Client(User):
-    tier = 'inactive'
-    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
-    def create_reservation(title, date_time, capacity):
-        return Reservation(title, date_time, capacity)
-    def __str__(self):
-        return self.name
-
 class Customer(User):
     def __str__(self):
         return self.name
@@ -25,3 +15,11 @@ class Reservation(models.Model):
     title = models.CharField(max_length=450)
     date_time = models.DateTimeField('date and time of the event')
     capacity = models.IntegerField(default=0)
+
+class Client(User):
+    tier = 'inactive'
+    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+    def create_reservation(title, date_time, capacity):
+        return Reservation(title, date_time, capacity)
+    def __str__(self):
+        return self.name
