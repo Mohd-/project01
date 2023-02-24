b8c4ad4a327fc7775e668060f4824aa8c9e583e1
mohd- <shinuobu@gmail.com>
2023-02-24 09:58:26 +0300

    modified:   mysite/db.sqlite3 modified:   mysite/mysite/settings.py new file:   mysite/tickets/migrations/0001_initial.py modified:   mysite/tickets/models.py

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
-class User(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone_no = models.CharField(max_length=200)
     def __str__(self):
         return self.name
 
-class Customer(User):
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone_no = models.CharField(max_length=200)
+    tier = models.CharField(max_length=200)
     def __str__(self):
         return self.name
     
@@ -15,11 +19,12 @@ class Reservation(models.Model):
     title = models.CharField(max_length=450)
     date_time = models.DateTimeField('date and time of the event')
     capacity = models.IntegerField(default=0)
-
-class Client(User):
-    tier = models.CharField(max_length=200)
-    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
-    def create_reservation(title, date_time, capacity):
-        return Reservation(title, date_time, capacity)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return self.title
+
+class Ticket(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.reservation.title
