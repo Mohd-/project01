16e70e8c32ec3dcd504b113f59eaf6655a2c90bb
mohd- <shinuobu@gmail.com>
2023-02-25 08:50:14 +0300

    modified:   mysite/db.sqlite3 	modified:   mysite/tickets/admin.py 	new file:   mysite/tickets/migrations/0002_remove_client_email_remove_client_name_and_more.py 	modified:   mysite/tickets/models.py

--- mysite/tickets/admin.py
+++ mysite/tickets/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import Client, Customer, Reservation
 
 admin.site.register(Reservation)
+admin.site.register(Client)
+admin.site.register(Customer)
--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractBaseUser
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     #name = models.CharField(max_length=200)
     #email = models.CharField(max_length=200)
     phone_no = models.CharField(max_length=200)
@@ -10,7 +10,7 @@ class Customer(models.Model):
         return self.user.username
 
 class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     #name = models.CharField(max_length=200)
     #email = models.CharField(max_length=200)
     phone_no = models.CharField(max_length=200)
