b93e57c36cff827c23b54e6ea2d3b30364037dd7
mohd- <shinuobu@gmail.com>
2023-02-26 12:29:38 +0300

    modified:   mysite/tickets/views.py

--- mysite/tickets/views.py
+++ mysite/tickets/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignupForm
 from django.http import HttpResponse
 from django.template import loader
 from .models import Client, Customer
@@ -6,10 +8,19 @@ from .models import Client, Customer
 def index(request):
     return HttpResponse("This is the main page")
 
-def register(request):
-    #template = loader.get_template('tickets/register.html')
-    context = {
-        Client : 'client',
-        Customer : 'customer',
-    }
-    return render(request,'tickets/register.html', context)
+
+
+def register(request): #should be signup
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if form.cleaned_data['type'] == 'client':
+                Client.objects.create(user=user)
+            else:
+                Customer.objects.create(user=user)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignupForm()
+    return render(request, 'tickets/register.html', {'form': form})
