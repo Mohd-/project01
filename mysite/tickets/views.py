c5ce85c01b50e683e58d639958de8ff8c1386f82
mohd- <shinuobu@gmail.com>
2023-02-26 12:12:20 +0300

    modified:   mysite/tickets/views.py

--- mysite/tickets/views.py
+++ mysite/tickets/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Client, Customer
 
 def index(request):
     return HttpResponse("This is the main page")
 
 def register(request):
-    return HttpResponse('sign up here!')
+    #template = loader.get_template('tickets/register.html')
+    context = {
+        Client : 'client',
+        Customer : 'customer',
+    }
+    return render(request,'tickets/register.html', context)
