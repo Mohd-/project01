30dccc1e12d46e4ad740dd26da7e1e3c4fd9e49a
Mohd- <shinuobu@gmail.com>
2023-02-27 22:42:52 +0300

    modified:   mysite/db.sqlite3 modified:   mysite/tickets/__pycache__/views.cpython-311.pyc new file:   mysite/tickets/migrations/0004_reservation_cost_alter_client_tier.py modified:   mysite/tickets/models.py

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -9,14 +9,20 @@ from django.contrib.auth.models import User, AbstractUser
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_no = models.CharField(max_length=200)
+
     def __str__(self):
         return self.user.username
 
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_no = models.CharField(max_length=200)
-    tierChoices = models.TextChoices('1','2','3')
-    tier = models.CharField(blank=True, choices=tierChoices, max_length=200)
+
+    class Tiers(models.TextChoices):
+        frst = '1', 'First'
+        scnd = '2', 'Second'
+        thrd = '3', 'Third'
+    tier = models.CharField(blank=True, choices=Tiers.choices, max_length=200)
+
     def __str__(self):
         return self.user.username
     
@@ -26,11 +32,13 @@ class Reservation(models.Model):
     capacity = models.IntegerField(default=0)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     cost = models.IntegerField(default=999999)
+
     def __str__(self):
         return self.title
 
 class Ticket(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.reservation.title
